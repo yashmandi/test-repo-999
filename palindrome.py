5953d2281343d6eeeeb3cbb57c9e07c3a6f3f519
yashmandi <yashmandi18@gmail.com>
2025-08-21 17:49:13 +0530

    Add palindrome check function

---
+++ palindrome.py
@@ -0,0 +1,9 @@
+def is_palindrome(s):
+    return s == s[::-1]
+
+# Example usage:
+word = "racecar"
+if is_palindrome(word):
+    print(f'{word} is a palindrome.')
+else:
+    print(f'{word} is not a palindrome.')
